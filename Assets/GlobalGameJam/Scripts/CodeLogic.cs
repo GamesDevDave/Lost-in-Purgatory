19c72a5f9f91a917d4e6e5a20664d380328e5952
Joe <jodeph.broughton@gmail.com>
2021-01-30 16:53:01 +0000

    -CodeChecker is now working and set up to use

--- Assets/GlobalGameJam/Scripts/CodeLogic.cs
+++ Assets/GlobalGameJam/Scripts/CodeLogic.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CodeLogic : MonoBehaviour
 {
@@ -8,12 +9,31 @@ public class CodeLogic : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.GetComponent<TextMeshProUGUI>().text = digit.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (digit == 10)
+        {
+            digit = 0;
+        }
+        if(digit == -1)
+        {
+            digit = 9;
+        }
+        this.GetComponent<TextMeshProUGUI>().text = digit.ToString();
+
+    }
+
+    public void DigitUp()
+    {
+        digit++;
+    }
+
+    public void DigitDown()
+    {
+        digit--;
     }
 }
